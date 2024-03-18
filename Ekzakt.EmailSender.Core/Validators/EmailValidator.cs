e2cef43138ecb9623a084b09be1024ec46138567
EricJansen <EricJansen@LAPTOP-EJ>
2024-03-18 13:34:08 +0100

    Abstracted away EmailValidator inside SendEmailRequestValidator.

---
+++ Ekzakt.EmailSender.Core/Validators/EmailValidator.cs
@@ -0,0 +1,35 @@
+﻿using Ekzakt.EmailSender.Core.Models;
+using FluentValidation;
+
+namespace Ekzakt.EmailSender.Core.Validators;
+
+public class EmailValidator: AbstractValidator<Email>
+{
+    public EmailValidator()
+    {
+        RuleFor(x => x.Sender)
+            .SetValidator(new EmailAddressValidator());
+
+        RuleFor(x => x.Tos.Count)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("At least one destination address (Tos) must be set.");
+
+        RuleForEach(x => x.Tos)
+            .NotNull()
+            .NotEmpty()
+            .SetValidator(new EmailAddressValidator());
+
+        RuleForEach(x => x.Ccs)
+            .SetValidator(new EmailAddressValidator());
+
+        RuleForEach(x => x.Bccs)
+            .SetValidator(new EmailAddressValidator());
+
+        RuleFor(x => x.Subject)
+            .NotNull()
+            .NotEmpty();
+
+        RuleFor(x => x.Body)
+            .SetValidator(new EmailBodyValidator());
+    }
+}
--- Ekzakt.EmailSender.Core/Validators/SendEmailRequestValidator.cs
+++ Ekzakt.EmailSender.Core/Validators/SendEmailRequestValidator.cs
@@ -15,29 +15,7 @@ public class SendEmailRequestValidator : AbstractValidator<SendEmailRequest>
             .NotNull()
             .NotEmpty();
 
-        RuleFor(x => x.Email.Sender)
-            .SetValidator(new EmailAddressValidator());
-
-        RuleFor(x => x.Email.Tos.Count)
-            .GreaterThanOrEqualTo(1)
-            .WithMessage("At least one destination address (Tos) must be set.");
-
-        RuleForEach(x => x.Email.Tos)
-            .NotNull()
-            .NotEmpty()
-            .SetValidator(new EmailAddressValidator());
-
-        RuleForEach(x => x.Email.Ccs)
-            .SetValidator(new EmailAddressValidator());
-
-        RuleForEach(x => x.Email.Bccs)
-            .SetValidator(new EmailAddressValidator());
-
-        RuleFor(x => x.Email.Subject)
-            .NotNull()
-            .NotEmpty();
-
-        RuleFor(x => x.Email.Body)
-            .SetValidator(new EmailBodyValidator());
+        RuleFor(x => x.Email)
+            .SetValidator(new EmailValidator());
     }
 }
